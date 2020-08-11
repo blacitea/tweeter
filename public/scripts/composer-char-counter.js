f2e6e66a07126c9d4e775c3c73d35d9ca79a9cb8
blacitea <angelhoikei@gmail.com>
2020-08-11 16:50:09 +0000

    create char-counter logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
   // --- our code goes here ---
   console.log("DOM is ready");
+  $('textarea').keyup(function() {
+    let charRemain = 140 - this.value.length;
+    if (charRemain < 0) {
+      $('output').css("color", "red");
+    } else {
+      $('output').removeAttr("style");
+    }
+    $('output').html(140 - this.value.length);
+  });
 });
