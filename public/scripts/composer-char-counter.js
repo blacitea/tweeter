acf0513a6e070a87520c82250b8cbc4efd450c48
blacitea <angelhoikei@gmail.com>
2020-08-11 18:22:43 +0000

    use this to retreive info and link to counter, instead of direct call .counter by selector

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log("DOM is ready");
   $('textarea').keyup(function() {
-    let charRemain = 140 - this.value.length;
-    if (charRemain < 0) {
-      $('output').css("color", "red");
-    } else {
-      $('output').removeAttr("style");
-    }
-    $('output').html(140 - this.value.length);
+    let charRemain = 140 - $(this).val().length;
+    let counter = $(this).siblings().children('.counter');
+    charRemain < 0 ? counter.css("color", "red") : counter.removeAttr("style");
+    counter.html(charRemain);
   });
 });
