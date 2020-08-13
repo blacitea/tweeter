612cb446da9b76302693cac84e70ff7c251816ea
blacitea <angelhoikei@gmail.com>
2020-08-13 05:31:12 +0000

    fix count for space/enter keys

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log("DOM is ready");
   $('textarea').keyup(function() {
-    let charRemain = 140 - $(this).val().length;
+    let charRemain = 140 - $(this).val().trim().length;
     let counter = $(this).siblings().children('.counter');
     charRemain < 0 ? counter.css("color", "red") : counter.removeAttr("style");
     counter.html(charRemain);
